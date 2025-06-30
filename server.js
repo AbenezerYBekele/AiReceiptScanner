a070c4bdf2616472c7f7d0381a10657e3b313db4
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-06-30 15:53:46 -0500

    Update server.js
    3

--- server.js
+++ server.js
@@ -23,7 +23,8 @@ app.get('/', (_req, res) => {
 });
 
 
-app.use("/api/transactions", transactionsRoute);
+//app.use("/api/transactions", transactionsRoute);
+app.use("/summary/:userId", TransactionSummary);
 
 let server;
 
