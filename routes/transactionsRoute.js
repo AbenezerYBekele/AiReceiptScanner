f51c4d7965ca9d8c494a8bee8460ccf2d8f6c2a7
Abenezer Y Bekele <113382918+AbenezerYBekele@users.noreply.github.com>
2025-06-30 20:31:49 -0500

    Update transactionsRoute.js
    5

--- routes/transactionsRoute.js
+++ routes/transactionsRoute.js
@@ -1,16 +1,17 @@
 import express from 'express';
-
 import {getTransactionsbyUserId, searchTransactions, createTransaction, deleteTransaction, getTransactionSummary } from '../controllers/transactionsController.js';
 
-
-
 export const router = express.Router();
-
 
 router.get("/:userId", getTransactionsbyUserId);
 router.get("/", searchTransactions);
 router.post("/", createTransaction);
 router.delete("/:id", deleteTransaction);
 router.get("/summary/:userId", getTransactionSummary);
+// router.get("/:userId", getTransactionsbyUserId);
+// router.get("/", searchTransactions);
+// router.post("/", createTransaction);
+// router.delete("/:id", deleteTransaction);
+// router.get("/summary/:userId", getTransactionSummary);
 
 export default router;
